d8eaf50d43cc9ca63d8e878ad6374572932cfb74
Sullivan Yang <kylin0521@gmail.com>
2024-11-04 05:23:31 +0000

    🎨 优化(room-detail-card.tsx)：简化游戏URI生成，移除条件前缀   💡 说明：去掉对QQ用户代理的检查，使生成的游戏URI更加一致。

--- components/room-detail-card.tsx
+++ components/room-detail-card.tsx
@@ -129,8 +129,7 @@ export default function RoomDetailCard({ roomId, heroes }: RoomDetailCardProps) 
       gameConfig.campid = team === "blue" ? "1" : "2"
       toast.success(`成功加入${team === "blue" ? "蓝队" : "红队"}, 等待跳转至游戏...`)
       // 将gameConfig进行base64后拼接
-      const prefix = navigator.userAgent.includes("QQ/") ? "https://h5.nes.smoba.qq.com/pvpesport.web.user/#/launch-game-mp-qq" : "tencentmsdk1104466820://"
-      const gameURI = `${prefix}?gamedata=SmobaLaunch_${encodeURIComponent(JSON.stringify(gameConfig))}`
+      const gameURI = `https://h5.nes.smoba.qq.com/pvpesport.web.user/#/launch-game-mp-qq?gamedata=SmobaLaunch_${encodeURIComponent(JSON.stringify(gameConfig))}`
       if (window.top) {
         window.top.location.href = gameURI
       } else {
